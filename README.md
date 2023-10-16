d6ea70140a617fab73434d3461601d9901f0fc9e
Antonio <antomanjavacas@gmail.com>
2023-10-16 10:00:47 +0200

    Data, plots, scripts, bib and model added

--- README.md
+++ README.md
@@ -1,3 +1,40 @@
 # A MELCOR model of the IFMIF-DONES SGS Argon Purification Subsystem
 
-(TO-DO)
+Supplementary material to the paper. Includes data, plots and additional code.
+
+The experiments were conducted using [MELCOR](https://www.sandia.gov/MELCOR/).
+
+## 📖 Abstract
+
+[IFMIF-DONES](https://ifmif-dones.es/) is an experimental facility designed to irradiate material samples under conditions similar to those expected in future fusion power reactors. As a radiological facility, its inherent safety risks require the precise design of specific subsystems responsible for the protection of the plant personnel and the environment. This is the case of the Argon Purification Subsystem (ArPS), which is responsible for ensuring the inerting of areas with the potential presence of liquid Li. However, design decisions regarding impurity requirements and purification parameters are complex, as many variables must be considered and optimized. To support this design process, a [MELCOR](https://www.sandia.gov/MELCOR/) simulation model of the IFMIF-DONES ArPS has been developed. Using this model, we perform a sensitivity analysis to study the performance of this subsystem under different configurations. We study how to keep the impurity concentrations of the inert spaces stable during a full year of operation, while minimizing the risks of Li reactions and ensuring dynamic confinement. Transitions between maintenance and operational modes are also analyzed to minimize the costs associated with Ar consumption.
+
+## 🔑 Keywords
+
+*IFMIF-DONES*, *MELCOR*, *Nuclear fusion*, *Lithium*
+
+## 👥 Contributors
+
+* [Antonio Manjavacas](mailto:manjavacas@ugr.es)
+* [Manuel A. Vázquez-Barroso](mailto:manvazbar@ugr.es)
+* [Claudio Torregrosa-Martín](mailto:cltorregrosa@ugr.es)
+* [Francisco Martín-Fuertes](mailto:francisco.martin-fuertes@ciemat.es)
+
+University of Granada, Centro de Investigaciones Energéticas, Medioambientales y Tecnológicas (CIEMAT).
+
+## 📝 Citation
+
+```
+@article{...}
+```
+
+## 📚 Bibliography
+
+Click [here](bibliography.bib) to view/download the bibliography of the paper.
+
+## 📄 License
+
+Read [LICENSE](./LICENSE).
+
+## 🪙 Funding
+
+...
---
+++ scripts/plots.R
@@ -0,0 +1,186 @@
+##########################################################################
+##                                                                      ##
+##  A MELCOR model of the IFMIF-DONES SGS Argon Purification Subsystem  ##
+##                                                                      ##
+##  Authors: A. Manjavacas                                              ##
+##                                                                      ##
+##  Copyright (c) Antonio Manjavacas, 2023                              ##
+##  Contact: manjavacas@ugr.es                                          ##
+##                                                                      ##
+##########################################################################
+
+library(tidyverse)
+library(ggplot2)
+library(grid)
+
+###################### FUNCTIONS ######################
+
+plot_cases <- function(df, filename) {
+  # N2 + O2 barplot
+  p1 <- ggplot(df, aes(x = df$Scenario)) +
+    geom_bar(aes(y = df$N2, fill = "N2"),
+      stat = "identity", position = "dodge"
+    ) +
+    geom_bar(aes(y = df$O2, fill = "O2"),
+      stat = "identity", position = "dodge"
+    ) +
+    geom_text(aes(y = df$N2, label = df$N2),
+      vjust = ifelse(df$Scenario == "(B1)", -1.5, -0.4),
+      position = position_dodge(width = 0.9)
+    ) +
+    geom_text(aes(y = df$O2, label = df$O2),
+      vjust = -0.5,
+      position = position_dodge(width = 0.9)
+    ) +
+    labs(
+      x = "", fill = "",
+      y = expression(paste("N"[2], ", ", "O"[2], " ", "(% vol.)"))
+    ) +
+    scale_fill_manual(values = c("N2" = "orange", "O2" = "red")) +
+    theme_linedraw()
+
+  # H2O barplot
+  p2 <- ggplot(df, aes(x = df$Scenario, y = df$H2O)) +
+    geom_bar(stat = "identity", fill = "navy", position = "dodge") +
+    geom_text(aes(label = df$H2O),
+      vjust = -0.5,
+      position = position_dodge(width = 0.9)
+    ) +
+    labs(x = "", y = expression(paste("H"[2], "O", " ", "(ppm)"))) +
+    theme_linedraw()
+
+  # VR barplot
+  p3 <- ggplot(df, aes(x = df$Scenario, y = df$VR)) +
+    geom_bar(stat = "identity", fill = "#137539", position = "dodge") +
+    geom_text(aes(label = df$VR),
+      vjust = -0.5,
+      position = position_dodge(width = 0.9)
+    ) +
+    labs(x = "", y = expression(paste("V"[R], " ", "(m"^3, ")"))) +
+    theme_linedraw()
+
+  p <- ggpubr::ggarrange(p1, p2, p3,
+    ncol = 3, nrow = 1,
+    legend = "top"
+  )
+  p <- ggpubr::annotate_figure(p,
+    bottom = textGrob("Scenario", gp = gpar(cex = 0.8))
+  )
+
+  ggsave(filename, plot = p, width = 12, height = 4.8, dpi = 400, bg = "white")
+}
+
+plot_impurities <- function(df, filename) {
+  df1 <- df %>% filter(grepl("^\\(C[1-4]\\)", df$Scenario))
+  df2 <- df %>% filter(grepl("^\\(C[5-8]\\)", df$Scenario))
+  df3 <- df %>% filter(grepl("^\\(C(9|1[0-2])\\)", df$Scenario))
+
+  df3$Scenario <- factor(df3$Scenario,
+    levels = c("(C9)", "(C10)", "(C11)", "(C12)")
+  )
+
+  p1 <- ggplot(df1, aes(x = df1$Scenario, y = 100 - df1$Ar)) +
+    geom_bar(stat = "identity", fill = "navy") +
+    labs(title = "1 day, 1 VPSA", x = "", y = "") +
+    geom_text(aes(label = round(100 - df1$Ar, 2)),
+      vjust = 1.5,
+      position = position_dodge(width = 0.9),
+      color = "white"
+    ) +
+    theme_linedraw()
+  p2 <- ggplot(df2, aes(x = df2$Scenario, y = 100 - df2$Ar)) +
+    geom_bar(stat = "identity", fill = "navy") +
+    labs(title = "1 day, 2 VPSA", x = "", y = "") +
+    geom_text(aes(label = round(100 - df2$Ar, 2)),
+      vjust = 1.5,
+      position = position_dodge(width = 0.9),
+      color = "white"
+    ) +
+    theme_linedraw()
+  p3 <- ggplot(df3, aes(x = df3$Scenario, y = 100 - df3$Ar)) +
+    geom_bar(stat = "identity", fill = "navy") +
+    labs(title = "1 day, 1 VPSA (saturation)", x = "", y = "") +
+    geom_text(aes(label = round(100 - df3$Ar, 2)),
+      vjust = 1.5,
+      position = position_dodge(width = 0.9),
+      color = "white"
+    ) +
+    theme_linedraw()
+
+  p <- ggpubr::ggarrange(p1, p2, p3,
+    ncol = 3, nrow = 1
+  )
+  p <- ggpubr::annotate_figure(
+    p,
+    left = textGrob("Total impurities (% vol.)",
+      rot = 90,
+      vjust = 1, gp = gpar(cex = 0.8)
+    ),
+    bottom = textGrob("Scenario", gp = gpar(cex = 0.8))
+  )
+
+  ggsave(filename, plot = p, width = 10, height = 3, dpi = 400, bg = "white")
+}
+
+plot_pressures <- function(df, filename) {
+  df$med <- 0.5 * (df$MinP + df$MaxP)
+  p <- ggplot(
+    df,
+    aes(
+      x = Scenario, ymin = MinP, lower = MinP,
+      middle = med, upper = MaxP, ymax = MaxP
+    )
+  ) +
+    geom_boxplot(stat = "identity", fill = "lightgray") +
+    labs(x = "Scenario", y = "Pressure (Pa)") +
+    theme_linedraw()
+
+  ggsave(filename, plot = p, width = 10, height = 6, dpi = 300, bg = "white")
+}
+
+# Load data
+scenarios <- read_csv("./data/scenarios.csv")
+
+###################### A SCENARIOS ######################
+
+cases_a1_to_a3 <- scenarios %>%
+  filter(grepl("^\\(A[1-3]\\)", Scenario))
+cases_a4_to_a6 <- scenarios %>%
+  filter(grepl("^\\(A[4-6]\\)", Scenario))
+cases_a7_to_a9 <- scenarios %>%
+  filter(grepl("^\\(A[7-9]\\)", Scenario))
+
+plot_cases(cases_a1_to_a3, "./plots/cases_a1_to_a3.png")
+plot_cases(cases_a4_to_a6, "./plots/cases_a4_to_a6.png")
+plot_cases(cases_a7_to_a9, "./plots/cases_a7_to_a9.png")
+
+###################### B SCENARIOS ######################
+
+cases_b1_to_b3 <- scenarios %>%
+  filter(grepl("^\\(B[1-3]\\)", Scenario))
+
+plot_cases(cases_b1_to_b3, "./plots/cases_b1_to_b3.png")
+
+###################### C SCENARIOS ######################
+
+cases_c1_to_c12 <- scenarios %>%
+  filter(grepl("^\\(C([1-9]|1[0-2])\\)", Scenario))
+
+plot_impurities(cases_c1_to_c12, "./plots/cases_c1_to_c12.png")
+
+####################### PRESSURES #######################
+
+# Load data
+pressures <- read_csv("./data/pressures.csv")
+
+a_pressures <- pressures %>%
+  filter(grepl("^\\(A[1-9]\\)", Scenario))
+b_pressures <- pressures %>%
+  filter(grepl("^\\(B[1-3]\\)", Scenario))
+c_pressures <- pressures %>%
+  filter(grepl("^\\(C([1-9]|1[0-2])\\)", Scenario)) %>%
+  mutate(Scenario = factor(Scenario, levels = paste0("(C", 1:12, ")")))
+
+plot_pressures(a_pressures, "./plots/a_pressures.png")
+plot_pressures(b_pressures, "./plots/b_pressures.png")
+plot_pressures(c_pressures, "./plots/c_pressures.png")
